bc27e4b48e9f8f3e3d70fc8f70af8b82536f8f4d
Nek-12 <vaizin.nikita@gmail.com>
2026-07-10 20:55:49 +0200

    feat: define generic PTY session commands

--- internal/testharness/pty/driver/types.go
+++ internal/testharness/pty/driver/types.go
@@ -1,10 +1,61 @@
 package driver
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"core/internal/testharness/pty/analyzer"
+
+	"github.com/google/uuid"
 )
+
+type SessionSpec struct {
+	Path       string
+	Args       []string
+	Env        []string
+	Dir        string
+	Dimensions analyzer.Dimensions
+}
+
+type SessionCommandKind int
+
+const (
+	SessionCommandWrite SessionCommandKind = iota + 1
+	SessionCommandResize
+	SessionCommandRuntimeControlByte
+	SessionCommandTerminateProcess
+)
+
+type SessionCommand struct {
+	ID         uuid.UUID
+	Kind       SessionCommandKind
+	Bytes      []byte
+	Dimensions *analyzer.Dimensions
+}
+
+func (command SessionCommand) Validate() error {
+	if command.ID == uuid.Nil || command.ID.Version() != 4 {
+		return fmt.Errorf("session command id must be UUIDv4")
+	}
+	switch command.Kind {
+	case SessionCommandWrite, SessionCommandRuntimeControlByte:
+		if len(command.Bytes) == 0 {
+			return errors.New("write command bytes are required")
+		}
+	case SessionCommandResize:
+		if command.Dimensions == nil {
+			return errors.New("resize command dimensions are required")
+		}
+		if _, err := analyzer.NewDimensions(command.Dimensions.Rows, command.Dimensions.Cols); err != nil {
+			return err
+		}
+	case SessionCommandTerminateProcess:
+	default:
+		return fmt.Errorf("unknown session command kind %d", command.Kind)
+	}
+	return nil
+}
 
 type CommandSpec struct {
 	Path            string
