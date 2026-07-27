c9d9f36268e9986844fd1d49d45fbadcdf9a951b
Nek-12 <vaizin.nikita@gmail.com>
2026-07-27 05:40:51 +0200

    feat: synchronize task search source edits

--- server/metadata/migrations/00061_task_search_triggers.up.sql
+++ server/metadata/migrations/00061_task_search_triggers.up.sql
@@ -63,3 +63,49 @@ BEGIN
       AND source_kind = 'title';
 END;
 -- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TRIGGER task_search_task_body_before_update
+BEFORE UPDATE OF body ON tasks
+BEGIN
+    INSERT INTO task_search_fts(task_search_fts, rowid, title, body, comment)
+    SELECT 'delete', document_id, NULL, OLD.body, NULL
+    FROM task_search_documents
+    WHERE task_id = OLD.id
+      AND source_kind = 'body';
+END;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TRIGGER task_search_task_body_after_update
+AFTER UPDATE OF body ON tasks
+BEGIN
+    INSERT INTO task_search_fts(rowid, title, body, comment)
+    SELECT document_id, NULL, NEW.body, NULL
+    FROM task_search_documents
+    WHERE task_id = NEW.id
+      AND source_kind = 'body';
+END;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TRIGGER task_search_comment_body_before_update
+BEFORE UPDATE OF body ON task_comments
+BEGIN
+    INSERT INTO task_search_fts(task_search_fts, rowid, title, body, comment)
+    SELECT 'delete', document_id, NULL, NULL, OLD.body
+    FROM task_search_documents
+    WHERE comment_id = OLD.id;
+END;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TRIGGER task_search_comment_body_after_update
+AFTER UPDATE OF body ON task_comments
+BEGIN
+    INSERT INTO task_search_fts(rowid, title, body, comment)
+    SELECT document_id, NULL, NULL, NEW.body
+    FROM task_search_documents
+    WHERE comment_id = NEW.id;
+END;
+-- +goose StatementEnd
