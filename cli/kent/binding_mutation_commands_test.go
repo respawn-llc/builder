dc87b7e11cbf7d8d4b3a86262a1a4e60aae89208
Nek-12 <vaizin.nikita@gmail.com>
2026-07-31 17:42:09 +0200

    test: align CLI checks with transport validation

--- cli/kent/binding_mutation_commands_test.go
+++ cli/kent/binding_mutation_commands_test.go
@@ -173,37 +173,6 @@ func TestDetachUnexpectedIncompleteResponseEmitsJSONFailure(t *testing.T) {
 	}
 }
 
-func TestProjectDefaultMalformedResponseEmitsJSONFailure(t *testing.T) {
-	assertBindingMutationJSONFailure(t, func(context.Context, *client.Remote, serverapi.ProjectWorkspaceSelector) (bindingMutationResult, error) {
-		return bindingMutationResult{Project: &serverapi.ProjectHomeSummary{}}, nil
-	}, true)
-}
-
-func TestProjectDefaultBlankWorkflowFieldsEmitJSONFailure(t *testing.T) {
-	t.Run("blank workflow ID", func(t *testing.T) {
-		project := validProjectHomeSummaryForBindingMutationTest()
-		blank := ""
-		name := "Workflow"
-		project.DefaultWorkflowID = &blank
-		project.DefaultWorkflowName = &name
-		project.DefaultWorkflowValid = true
-		assertBindingMutationJSONFailure(t, func(context.Context, *client.Remote, serverapi.ProjectWorkspaceSelector) (bindingMutationResult, error) {
-			return bindingMutationResult{Project: &project}, nil
-		}, true)
-	})
-	t.Run("blank workflow name", func(t *testing.T) {
-		project := validProjectHomeSummaryForBindingMutationTest()
-		id := "workflow-1"
-		blank := ""
-		project.DefaultWorkflowID = &id
-		project.DefaultWorkflowName = &blank
-		project.DefaultWorkflowValid = true
-		assertBindingMutationJSONFailure(t, func(context.Context, *client.Remote, serverapi.ProjectWorkspaceSelector) (bindingMutationResult, error) {
-			return bindingMutationResult{Project: &project}, nil
-		}, true)
-	})
-}
-
 func TestLegacyEmptyProjectKeyMutationSuccessOutputs(t *testing.T) {
 	installBindingMutationTestRemote(t)
 	project := validProjectHomeSummaryForBindingMutationTest()
