ebe9a50075c89b2fe4369966b3becfd9c789bb25
Nek-12 <vaizin.nikita@gmail.com>
2026-08-10 15:23:20 +0200

    fix!: advance runtime command protocol

--- shared/protocol/task_initial_branch_version_test.go
+++ shared/protocol/task_initial_branch_version_test.go
@@ -2,8 +2,8 @@ package protocol
 
 import "testing"
 
-func TestWorkflowTaskInitialBranchChangesProtocolVersion(t *testing.T) {
-	if Version != "105" {
-		t.Fatalf("workflow task initial branch protocol version = %q, want 105 after merging the version 102 and version 104 contract histories", Version)
+func TestCurrentContractChangesProtocolVersion(t *testing.T) {
+	if Version != "106" {
+		t.Fatalf("current protocol version = %q, want 106", Version)
 	}
 }
