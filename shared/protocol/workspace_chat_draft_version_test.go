9b8c155ffdcd2dfe8667aa461f9b71e4223dab1a
Nek-12 <vaizin.nikita@gmail.com>
2026-08-10 03:17:10 +0200

    fix: align protocol version guard after rebase

--- shared/protocol/workspace_chat_draft_version_test.go
+++ shared/protocol/workspace_chat_draft_version_test.go
@@ -1,5 +1,5 @@
 package protocol
 import "testing"
 func TestWorkspaceChatDraftRaisesProtocolVersion(t *testing.T) {
-	if Version != "98" { t.Fatalf("version = %q", Version) }
+	if Version != "102" { t.Fatalf("version = %q", Version) }
 }
