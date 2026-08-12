b78ea7fefc95a0bfa5d16a274aaa9a6f2d4c22e3
Nek-12 <vaizin.nikita@gmail.com>
2026-08-13 01:34:55 +0200

    fix: format migration version constants

--- server/metadata/migrations/versions.go
+++ server/metadata/migrations/versions.go
@@ -5,5 +5,5 @@ const (
 	MinimumSupportedRelease                        = "v2.0.0"
 	WorkflowIdentityMigrationVersion         int64 = 62
 	WorkflowSessionAgentRoleMigrationVersion int64 = 63
-	WorkflowLegacyProvenanceRepairVersion     int64 = 85
+	WorkflowLegacyProvenanceRepairVersion    int64 = 85
 )
